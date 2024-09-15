115422612ac58ed5614797fd421d1d060fe7b508
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-09-15 14:19:19 +0200

    Added questions as comments and answered 6 and 8

--- HW1/script.sql
+++ HW1/script.sql
@@ -1,4 +1,8 @@
--- # Question 1:
+/* 
+ * Question 1:
+ * The person relation contains 284 entries with a registered death date after ‘2010-02-01’.
+ * How many entries do not have a registered death date?
+ */
 
 -- Example
 SELECT COUNT(*) AS person_count
@@ -10,7 +14,13 @@ SELECT COUNT(*) AS person_count
 FROM person
 WHERE deathdate IS NULL;
 
--- # Question 2:
+/*
+ * Question 2:
+ * In the database, there are 46 movies in the French language for which the average
+ * height of all the people involved is greater than 185 centimeters (ignoring people with
+ * unregistered height). What is the number of movies in the Portuguese language for
+ * which the average height of all people involved is greater than 175 centimeters?
+ */
 
 -- Example
 SELECT COUNT(*) AS movie_count
@@ -38,7 +48,14 @@ FROM (
 )
 AS subquery;
 
--- # Question 3
+/*
+ * Question 3
+ * The movie genre relation does not have a primary key, which can lead to a movie
+ * having more than one entry with the same genre. As a result, there are 14 movies
+ * in movie genre that have the genre ‘Action’ assigned to them more than once. How
+ * many movies in movie genre have the genre ‘Thriller’ assigned to them more than
+ * once?
+ */
 
 -- Example
 SELECT COUNT(*) AS movie_count
@@ -62,10 +79,15 @@ FROM (
 )
 AS subquery;
 
--- # Question 4
+/*
+ * Question 4
+ * According to the information in the database, 52 different people acted in movies
+ * directed by ‘Ingmar Bergman’. How many different people acted in movies directed
+ * by ‘Akira Kurosawa’?
+ */ 
 
 -- Example
-SELECT COUNT(DISTINCT p.id) AS "actor_count"
+SELECT COUNT(DISTINCT p.id) AS person_count
 FROM involved i 
 JOIN person p ON i.personid = p.id
 JOIN (
@@ -76,7 +98,7 @@ JOIN (
 ) m ON i.movieid = m.id;
 
 -- Correct
-SELECT COUNT(DISTINCT p.id) AS "actor_count"
+SELECT COUNT(DISTINCT p.id) AS person_count
 FROM involved i 
 JOIN person p ON i.personid = p.id
 JOIN (
@@ -86,10 +108,14 @@ JOIN (
 	WHERE i."role" = 'director' AND p."name" = 'Akira Kurosawa'
 ) m ON i.movieid = m.id;
 
--- # Question 5
+/*
+ * Question 5
+ * Of all the movies produced in 2007, there are 15 that have two directors involved
+ * in them. How many movies produced in 2010 have two directors involved in them?
+ */
 
 -- Example
-SELECT COUNT(*) AS "movie_count"
+SELECT COUNT(*) AS movie_count
 FROM (
 	SELECT COUNT(p.id) FROM involved i
 	JOIN person p ON i.personid = p.id
@@ -101,7 +127,7 @@ FROM (
 AS subquery;
 
 -- Correct
-SELECT COUNT(*) AS "movie_count"
+SELECT COUNT(*) AS movie_count
 FROM (
 	SELECT COUNT(p.id) FROM involved i
 	JOIN person p ON i.personid = p.id
@@ -112,65 +138,102 @@ FROM (
 )
 AS subquery;
 
--- # Question 6
+/*
+ * Question 6
+ * There are 406 unique pairs of actors who have appeared together in exactly 10 movies
+ * released between 2000 and 2010. How many unique pairs of actors have appeared
+ * together in exactly 20 movies released between 2000 and 2010?
+ */
 
 -- Example
-
+SELECT COUNT(*) FROM involved i
+JOIN person p ON i.personid = p.id
+JOIN movie m ON i.movieid = m.id
+WHERE m."year" BETWEEN 2000 AND 2010
 
 -- Correct
 
-
--- # Question 7
+/*
+ * Question 7
+ * Of all the movies produced between 2000 and 2002, there are 782 that have entries
+ * registered in involved for all roles defined in the roles relation. How many movies
+ * produced between 2002 and 2004 have entries registered in involved for all roles
+ * defined in the roles relation?
+ */
 
 -- Example
-SELECT COUNT(DISTINCT m.id) AS "movie_count"
-FROM movie m
-WHERE m.year BETWEEN 2000 AND 2002
-AND NOT EXISTS (
-    SELECT *
-    FROM role r
-    WHERE NOT EXISTS (
-        SELECT *
-        FROM involved i
-        WHERE i.movieid = m.id
-    )
+SELECT COUNT(*) AS movie_count
+FROM (
+	SELECT COUNT(DISTINCT i."role")
+	FROM involved i
+	JOIN person p ON i.personid = p.id
+	JOIN movie m ON i.movieid = m.id
+	WHERE m."year" BETWEEN 2000 AND 2002
+	GROUP BY m.id
+	HAVING COUNT(DISTINCT i."role") = (SELECT COUNT(*) FROM ROLE)
+)
+AS subquery;
+
+-- Correct
+SELECT COUNT(*) AS movie_count
+FROM (
+	SELECT COUNT(DISTINCT i."role")
+	FROM involved i
+	JOIN person p ON i.personid = p.id
+	JOIN movie m ON i.movieid = m.id
+	WHERE m."year" BETWEEN 2002 AND 2004
+	GROUP BY m.id
+	HAVING COUNT(DISTINCT i."role") = (SELECT COUNT(*) FROM ROLE)
+)
+AS subquery;
+
+/*
+ * Question 8
+ * The number of people who have played a role in movies of all genres in the category
+ * ‘Newsworthy’ is 156. How many people have played a role in movies of all genres
+ * in the category ‘Newsworthy’ but have not played any role in movies that cover all
+ * genres in the category ‘Popular’?
+ */
+
+-- Example
+SELECT COUNT(DISTINCT p.id) FROM involved i
+JOIN person p ON i.personid = p.id
+JOIN movie m ON i.movieid = m.id
+WHERE m.id IN (
+	SELECT m.id FROM movie_genre mg
+	JOIN movie m ON mg.movieid = m.id
+	JOIN genre g ON mg.genre = g.genre
+	WHERE g.category = 'Newsworthy'
+	GROUP BY m.id
+	HAVING COUNT(g.genre) = (
+		SELECT COUNT(g.genre) FROM genre g 
+		WHERE g.category = 'Newsworthy'
+	)
 );
 
 -- Correct
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT COUNT(DISTINCT p.id) FROM involved i
+JOIN person p ON i.personid = p.id
+JOIN movie m ON i.movieid = m.id
+WHERE m.id IN (
+	SELECT m.id FROM movie_genre mg
+	JOIN movie m ON mg.movieid = m.id
+	JOIN genre g ON mg.genre = g.genre
+	WHERE g.category = 'Newsworthy'
+	GROUP BY m.id
+	HAVING COUNT(g.genre) = (
+		SELECT COUNT(g.genre) FROM genre g 
+		WHERE g.category = 'Newsworthy'
+	)
+)
+AND p.id NOT IN (
+	SELECT m.id FROM movie_genre mg
+	JOIN movie m ON mg.movieid = m.id
+	JOIN genre g ON mg.genre = g.genre
+	WHERE g.category = 'Popular'
+	GROUP BY m.id
+	HAVING COUNT(g.genre) = (
+		SELECT COUNT(g.genre) FROM genre g 
+		WHERE g.category = 'Popular'
+	)
+);
